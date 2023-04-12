3028f87b52c8ff905e0d957fac1c24e88af63aa3
gingaminga <gingahae@gmail.com>
2023-04-13 01:39:38 +0900

    ✨ Feat: 인가코드 가져오기 구현
    백엔드에서 url 전달하는 API 작성
    클라이언트 button UI 구성
    응답받은 url로 카카오 로그인 하도록 기능 구현

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,4 +6,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+app.get("/kakao/url", (req, res, next) => {
+  const CLIENT_ID = process.env.CLIENT_ID;
+  const REDIRECT_URI = "http://localhost:3000/callback/kakao";
+
+  const url = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+  res.status(200).json({
+    url,
+  });
+});
+
 export default app;
