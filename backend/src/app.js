2cd571539974beb2b5a74b5bbd2236afbbdbaf3a
gingaminga <gingahae@gmail.com>
2023-04-13 17:12:43 +0900

    ♻️ Refactor: kakao 모듈화
    kakao 관련 모듈화
    env.sample - change kakao key property name

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { KakaoClient } from "./kakao.js";
 
 const app = express();
 app.use(express.json());
@@ -7,10 +8,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.get("/kakao/url", (req, res, next) => {
-  const CLIENT_ID = process.env.CLIENT_ID;
-  const REDIRECT_URI = "http://localhost:3000/callback/kakao";
-
-  const url = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+  const url = KakaoClient.getAuthCodeURL();
 
   res.status(200).json({
     url,
