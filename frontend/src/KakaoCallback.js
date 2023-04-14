9101ca9bc4bb2676b8d5edd8ad0d032af0fd858f
gingaminga <gingahae@gmail.com>
2023-04-14 16:40:41 +0900

    ✨ Feat: 로그인 요청 기능 개발
    버튼 클릭 시 code 상태 변경
    code가 있을 경우 API fetch
    성공 시 화면 전환

--- frontend/src/KakaoCallback.js
+++ frontend/src/KakaoCallback.js
@@ -1,45 +1,61 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function KakaoCallback() {
   const [code, setCode] = useState("");
+  const navigate = useNavigate();
+
   /**
-   * @description 로그인/회원가입 하기
+   * @description 로그인하기
    */
-  const fetchLogin = async (code) => {
-    try {
-      const { isGood } = await (
-        await fetch("http://localhost:3001/login", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ code }),
-        })
-      ).json();
+  const fetchLogin = useCallback(
+    async (code) => {
+      try {
+        const param = {
+          code,
+        };
 
-      console.log(isGood);
-    } catch (error) {
-      alert("Function fetchLogin error!");
-      console.error(error);
-    }
-  };
+        const response = await (
+          await fetch("http://localhost:3001/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(param), // string으로 전달해야함
+          })
+        ).json();
 
+        console.log(response); // { nickname: '#######' }
+
+        navigate("/main"); // API 호출 성공 시 메인 페이지로 이동
+      } catch (error) {
+        alert("Function fetchLogin error!");
+        console.error(error);
+      }
+    },
+    [navigate]
+  );
+
+  /**
+   * @description login API fetch
+   */
   useEffect(() => {
     if (code) {
       fetchLogin(code);
     }
-  });
+  }, [code, fetchLogin]);
 
+  /**
+   * @description code 값 가져오기
+   */
   useEffect(() => {
     const Address = new URL(window.location.href);
-
     const code = Address.searchParams.get("code") || "";
 
-    // fetchLogin(code);
     setCode(code);
   }, []);
 
-  return <div>대기중..</div>;
+  return <div className="App">Wait....</div>;
 }
 
 export default KakaoCallback;
