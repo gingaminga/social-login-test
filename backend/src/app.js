5aee946a3e16f5a1216023ecdd77281f5b8a0c1c
gingaminga <gingahae@gmail.com>
2023-04-14 16:39:45 +0900

    💡 Style: 주석 추가

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,12 +11,12 @@ app.post("/login", async (req, res, next) => {
   try {
     const { code } = req.body;
 
-    const { access_token, refresh_token } = await KakaoClient.getToken(code); // 토큰 받아오기
+    const { access_token } = await KakaoClient.getToken(code); // 토큰 받아오기
     const userData = await KakaoClient.getUserData(access_token); // 유저 정보 받아오기
 
-    // 우리 서비스 로그인/회원가입 처리
-    // 우리 서비스 토큰 발급
-    // 등등 처리 해줘야 함..
+    // 그 후 DB로 사용자 등록 처리
+    // 세션 or 토큰 처리
+    // 등등 로그인 관련 처리를 해줘야 함
 
     res.status(200).json(userData);
   } catch (error) {
